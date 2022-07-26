d872207881b1c81ae2fcd15d8fa838969db5b85e
drpviray <drpviray@donbosco.edu.ph>
2022-07-26 11:10:13 +0800

    Initial Commit

---
+++ settings.php
@@ -0,0 +1,13 @@
+<?php
+    $database = 'mysql:host=localhost; dbname=pocketguide';
+    $user = 'pocketguide-admin';
+    $pwd = '2RlD.6/8hj.PfsU(';
+
+    try{
+        $pdo = new PDO($database, $user, $pwd);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //echo 'Connection success!';
+    }catch(PDOException $e){
+        echo 'Connection failed: '.$e->getMessage();
+    }
+?>
---
+++ user_accounts.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 12, 2022 at 06:02 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pocketguide`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_accounts`
+--
+
+CREATE TABLE `user_accounts` (
+  `uid` int(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `fullname` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `user_type` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_accounts`
+--
+
+INSERT INTO `user_accounts` (`uid`, `email`, `fullname`, `password`, `user_type`) VALUES
+(1, 'pocketguide-admin', '', '2RlD.6/8hj.PfsU(', '1'),
+(2, 'deniel@gmail.com', '', 'adad', '1'),
+(3, 'viray', 'pogi', 'cute', '1'),
+(4, 'viray', 'de', '123', '1'),
+(5, 'viray', '1234', '1234', '1'),
+(6, 'den', 'deniel', '1234', '1');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user_accounts`
+--
+ALTER TABLE `user_accounts`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user_accounts`
+--
+ALTER TABLE `user_accounts`
+  MODIFY `uid` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
